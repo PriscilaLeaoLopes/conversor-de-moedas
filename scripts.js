526dfcdb6ef5b058f5ccd76f16303e83a80dc0f9
PriscilaLeaoLopes <priaparecidaleao@gmail.com>
2023-09-14 12:59:49 -0300

    alterado moedas

--- scripts.js
+++ scripts.js
@@ -8,7 +8,8 @@ function convertValues(){
 
     const dolarToday = 4.9
     const euroToday = 5.3
-
+    const bitcointoday = 127.884
+    const libratoday = 6.58
 
 if(currencySelect.value == "dolar"){
     moedaValorConvert.innerHTML = new Intl.NumberFormat("en-US",{
@@ -25,6 +26,22 @@ if(currencySelect.value == "euro"){
         currency: "EUR"
     }).format(inputCurrencyValue / euroToday)
 }
+
+
+if(currencySelect.value == "bitcoin"){
+    moedaValorConvert.innerHTML = new Intl.NumberFormat("de-DE",{     /* criado agora      */
+        style:"currency",
+        currency: "BTC"
+    }).format(inputCurrencyValue / bitcointoday)
+}
+
+if(currencySelect.value == "libra"){
+    moedaValorConvert.innerHTML = new Intl.NumberFormat("de-DE",{     /* criado agora      */
+        style:"currency",
+        currency: "GBP"
+    }).format(inputCurrencyValue / libratoday)
+}
+
  
     moedaValorToConvert.innerHTML =  new Intl.NumberFormat("pt-br",{
         style: "currency",
@@ -49,7 +66,17 @@ function changeCurrency(){
         currencyImagem.src = "./assets/euro.png"
     }
 
-     g
+    if (currencySelect.value == "bitcoin"){
+        currencyName.innerHTML = "bitcoin"
+        currencyImagem.src = "./assets/bitcoin 1.png"   /* agora   */
+    }
+
+    if (currencySelect.value == "libra"){
+        currencyName.innerHTML = "libra"
+        currencyImagem.src = "./assets/libra 1.png"   /* agora   */
+    }
+
+     
 }
  
 
